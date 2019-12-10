da6d2b1d623506964f24c3dedf4f1b079b320039
Kah919 <kahyap13@gmail.com>
2019-12-10 04:08:00 -0500

    add DOM element

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,1 +1,9 @@
-// your code goes here ...
+const form = document.querySelector('form')
+const ageInput = document.querySelector('input[name="age"]');
+
+
+ageInput.addEventListener('change', event => {
+    event.preventDefault()
+    console.log(ageInput.value)
+})
+console.log(ageInput.value)
